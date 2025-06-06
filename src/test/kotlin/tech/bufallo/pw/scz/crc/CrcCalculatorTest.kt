ad21aac5bae90638bffbfb48c68b58f5b9a4cc09
bzadrozny-vimn <bartlomiej.zadrozny@paramount.com>
2025-06-06 23:50:49 +0200

    Extend tests error message

--- src/test/kotlin/tech/bufallo/pw/scz/crc/CrcCalculatorTest.kt
+++ src/test/kotlin/tech/bufallo/pw/scz/crc/CrcCalculatorTest.kt
@@ -10,10 +10,10 @@ class CrcCalculatorTest {
         @JvmStatic
         fun crcTestCases() = listOf(
             // message, expected CRC
-            byteArrayOf() to  0xFFFF,
+            byteArrayOf() to 0xFFFF,
             byteArrayOf(0x01) to 0x807E,
             byteArrayOf(0x01, 0xAB.toByte(), 0x1D, 0x01, 0xFF.toByte(), 0x1) to 0x8EF3,
-            byteArrayOf(0x01, 0x10, 0x00, 0x11, 0x00, 0x03, 0x06, 0x1A, 0xC4.toByte(), 0xBA.toByte(), 0xD0.toByte()) to  0x677F,
+            byteArrayOf(0x01, 0x10, 0x00, 0x11, 0x00, 0x03, 0x06, 0x1A, 0xC4.toByte(), 0xBA.toByte(), 0xD0.toByte()) to 0x677F,
         )
     }
 
@@ -21,7 +21,13 @@ class CrcCalculatorTest {
     @MethodSource("crcTestCases")
     fun `should calculate correct CRC for given input`(testCase: Pair<ByteArray, Int>) {
         val (message, expectedCrc) = testCase
+        println("Testing message: ${message.joinToString(", ") { "0x%02X".format(it) }} with expected CRC: 0x%04X".format(expectedCrc))
+
         val actualCrc = CrcCalculator.calculate(message)
-        assertEquals(expectedCrc, actualCrc)
+        assertEquals(
+            expectedCrc,
+            actualCrc,
+            "CRC mismatch. Expected CRC: 0x%04X. Actual CRC: 0x%04X".format(expectedCrc, actualCrc)
+        )
     }
 }
